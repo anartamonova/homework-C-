8a7ff786ee211cd8add2718fd1c2bad9ce88f188
Anna Artamonova <tw1ssss@yandex.ru>
2023-01-28 18:28:09 +0300

    Added Hw4

---
+++ hw4/task 27/Program.cs
@@ -0,0 +1,17 @@
+﻿//Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+//452 -> 11
+//82 -> 10
+//9012 -> 12
+
+
+Console.Write("Введите число: ");
+int  N = Convert.ToInt32(Console.ReadLine());
+int x = N;
+int sumX = 0;
+
+while (x > 0) {
+int numX = x % 10;
+x = x / 10;
+sumX  = sumX + numX;
+};
+Console.WriteLine($"{N} --> {sumX}");
---
+++ hw4/task29/Program.cs
@@ -0,0 +1,17 @@
+﻿//Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+//1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+//6, 1, 33 -> [6, 1, 33]
+
+Console.Write("Введите число элементов: ");
+int length = Convert.ToInt32(Console.ReadLine());
+  if (length <= 8) {
+    int[] array = new int[length];
+    for (int i = 0; i < array.Length; i++) {
+      array[i] = new Random().Next(1,12);
+      Console.Write(array[i]+ ", ");
+    }
+  }
+  else {
+        Console.Write("Количество элементов должно быть не больше 8");
+    }
+   
